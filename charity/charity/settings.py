8cb14132410e718e5996c6ebeec16e49a6d98265
MuhibullohHakimjonov <mukhabillo@gmail.com>
2025-11-22 12:01:19 +0500

    Updated sett

--- charity/charity/settings.py
+++ charity/charity/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&98vj&zx#5orwz)9(45nasv@(u1_1b%wrzokz_cxyu1_@1u6u0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
